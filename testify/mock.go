5d549cda370f77f0bb6d7c13b8e8909045722fcd
Andy McCall <andymc903@gmail.com>
2017-07-05 23:51:53 -0700

    added godocs

--- testify/mock.go
+++ testify/mock.go
@@ -2,6 +2,8 @@ package testify
 
 import "github.com/stretchr/testify/mock"
 
+// Mock is the assertion interface exposed by stretchr/testify. It exposes the various verification methods implemented
+// by the mock struct
 type Mock interface {
 	AssertCalled(t mock.TestingT, methodName string, args... interface{}) bool
 	AssertExpectations(t mock.TestingT) bool
